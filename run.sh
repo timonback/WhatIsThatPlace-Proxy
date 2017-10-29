3af41c07730985cf48475828e21f006c2d77e0ee
Timon Back <T.Back@student.rug.nl>
2017-10-29 19:29:01 +0100

    Update run script

--- run.sh
+++ run.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
 HOST=0.0.0.0
-PORT=8000
+PORT=8888
 
-python3.6 setup.py install
+#sudo apt-get install python3-pip python3-dev
+echo "Install requirements..."
+pip3 install virtualenv
+python3 -m virtualenv -p python3 env
+source env/bin/activate
+
+python3 setup.py install
 
 export GOOGLE_APPLICATION_CREDENTIALS=gkey.json
 
@@ -17,6 +23,7 @@ function server_windows() {
     waitress-serve --host="$HOST" -port="$PORT" "server.app:get_app()"
 }
 
+echo "Start serving the application..."
 if [ "$(uname)" == "Darwin" ]; then
     server_unix
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
