63f4790dd343560097ac12a1f6012dc3273b1059
Timon Back <T.Back@student.rug.nl>
2017-11-17 00:30:20 +0100

    Added slashes

--- server/component/image_store.py
+++ server/component/image_store.py
@@ -58,9 +58,9 @@ class ImageStore(object):
         ext = None
         if filename is not None and len(filename) > 1:
             ext = os.path.splitext(filename)[1]
-        image_upload_path = '{folder}{uuid}{ext}'.format(folder=self._storage_path,
-                                                         uuid=self._uuidgen(),
-                                                         ext=ext)
+        image_upload_path = '{folder}/{uuid}{ext}'.format(folder=self._storage_path,
+                                                          uuid=self._uuidgen(),
+                                                          ext=ext)
         file_hash = hashlib.md5()
         with self._fopen(image_upload_path, 'wb') as image_file:
             while True:
@@ -72,9 +72,9 @@ class ImageStore(object):
                 file_hash.update(chunk)
 
         image_id = file_hash.hexdigest()
-        image_real_path = '{folder}{name}{ext}'.format(folder=self._storage_path,
-                                                       name=image_id,
-                                                       ext=ext)
+        image_real_path = '{folder}/{name}{ext}'.format(folder=self._storage_path,
+                                                        name=image_id,
+                                                        ext=ext)
         image_real_name = '{name}{ext}'.format(name=image_id, ext=ext)
 
         if self._db:
